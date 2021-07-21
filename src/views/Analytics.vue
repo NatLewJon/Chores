c3865a1dbd1be3a8af8c0fe8655ef1e45616f495
SamWalker251 <55162645+SamWalker251@users.noreply.github.com>
2021-07-21 23:34:52 +0100

    Analytics screen
    Added Analytics screen

---
+++ src/views/Analytics.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="analytics">
+    <h2>Analytics</h2>
+    <Graph/>
+  </div>
+</template>
+
+<script>
+import Graph from "../components/Graph.vue";
+
+export default {
+    name: 'analytics',
+    components: {Graph},
+    data: () => ({
+
+    })
+}
+</script>
+
+<style scoped>
+
+</style>
